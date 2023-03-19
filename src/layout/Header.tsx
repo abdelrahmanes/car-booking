56ccd8f79aaf1b42eebd4c77dcad7206d1229358
abdelrahmanes <abdelrahmanelshershaby@gmail.com>
2023-03-19 07:24:34 +0200

    replace the nav dropdown with drawer

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,6 +5,9 @@ import {
   Avatar,
   Flex,
   Burger,
+  Drawer,
+  Button,
+  Group,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { NavLink } from "react-router-dom";
@@ -72,7 +75,7 @@ interface HeaderProps {
 export default function Header({ user }: HeaderProps) {
   const { classes } = useStyles();
   const lang = localStorage.getItem("lang");
-  const [opened, { toggle }] = useDisclosure(false);
+  const [opened, { open, close }] = useDisclosure(false);
   const label = opened ? "Close navigation" : "Open navigation";
   const data = [
     {
@@ -87,30 +90,83 @@ export default function Header({ user }: HeaderProps) {
       key={item.label}
       to={item.link}
       className={({ isActive }) =>
-        isActive ? `${classes.linkActive} ${classes.link}` : classes.link
+        isActive
+          ? `${classes.linkActive} ${classes.link} ${
+              lang === "ar" ? "rtl" : "ltr"
+            }`
+          : `${classes.link} ${lang === "ar" ? "rtl" : "ltr"}`
       }
-      onClick={toggle}
+      onClick={close}
     >
       <item.icon className={classes.linkIcon} stroke={"1.5"} />
       <span>{item.label}</span>
     </NavLink>
   ));
   return (
-    <div className={`${classes.header} flex-1  bg-white md:sticky top-0 z-50`}>
+    <div className={`${classes.header}     bg-white md:sticky top-0 z-50`}>
       <Container className={classes.mainSection} p={0}>
-        <Flex align={"center"} justify={"space-between"} gap={6}>
-          <Flex align={"center"} gap={8}>
-            <Burger
-              display={{ xxs: "block", sm: "none" }}
+        <Flex
+          align={"center"}
+          justify={"space-between"}
+          gap={6}
+          className={`${lang === "ar" ? " flex-row-reverse" : "flex-row "}`}
+        >
+          <Flex
+            align={"center"}
+            gap={8}
+            className={`${lang === "ar" ? "  flex-row-reverse" : "flex-row"}`}
+          >
+            <Drawer
               opened={opened}
-              onClick={toggle}
-              aria-label={label}
-              color={"#5F6165"}
-              pos={"relative"}
-            />
+              onClose={close}
+              position={lang === "ar" ? "right" : "left"}
+              overlayProps={{ opacity: 0.2, blur: 4 }}
+              size={"xs"}
+              className="flex flex-col justify-between gap-12"
+            >
+              <Flex direction={"column"} gap={3} className="pb-8">
+                {links}
+              </Flex>
+              <Flex
+                direction={"column"}
+                gap={3}
+                className="border-t border-gray-200 pt-1"
+              >
+                <a
+                  href="#"
+                  className={`${classes.link} ${lang === "ar" ? "rtl" : "ltr"}`}
+                  onClick={(event) => event.preventDefault()}
+                >
+                  <SettingsIcon className={classes.linkIcon} stroke={"1.5"} />
+                  <span>Settings</span>
+                </a>
 
-            <div
-              className={`absolute  top-20 left-2  z-50 bg-white rounded-lg ${
+                <a
+                  href="#"
+                  className={`${classes.link} ${lang === "ar" ? "rtl" : "ltr"}`}
+                  onClick={(event) => event.preventDefault()}
+                >
+                  <LogoutIcon className={classes.linkIcon} stroke={"1.5"} />
+                  <span>Logout</span>
+                </a>
+              </Flex>
+            </Drawer>
+
+            <Group position="center">
+              <Burger
+                display={{ xxs: "block", sm: "none" }}
+                opened={opened}
+                onClick={open}
+                aria-label={label}
+                color={"#5F6165"}
+                pos={"relative"}
+              />
+            </Group>
+
+            {/* <div
+              className={`absolute ${
+                lang === "ar" ? "right-2" : "left-2"
+              } top-20 left-2  z-50 bg-white rounded-lg ${
                 opened ? "w-64 p-2" : "w-0 overflow-hidden p-0"
               } transition-width duration-150`}
             >
@@ -140,12 +196,16 @@ export default function Header({ user }: HeaderProps) {
                   <span>Logout</span>
                 </a>
               </Flex>
-            </div>
+            </div> */}
 
             <SearchInput />
           </Flex>
 
-          <Flex gap={{ xxs: 6, md: 22 }} align={"center"}>
+          <Flex
+            gap={{ xxs: 6, md: 22 }}
+            align={"center"}
+            className={`${lang === "ar" ? " flex-row-reverse" : " flex-row"}`}
+          >
             <NotificationIcon width={19.2} height={27} stroke={"1.5"} />
 
             <Avatar src={user.image} alt={user.name} radius="xl" size={48} />
